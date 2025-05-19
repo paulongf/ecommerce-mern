f4a051baccdf46bcf55087a65a4e43eacd8ef6c0
Paulongf <paulongf@hotmail.com>
2025-05-19 18:42:56 +0100

    Implementing Add Products

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth-slice'
+import authReducer from './auth-slice';
+import adminProductsSlice from './admin/products-slice'
 
 
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        adminProducts: adminProductsSlice,
     },
 });
 
