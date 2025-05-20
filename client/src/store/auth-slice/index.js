6aed75cef4b161396c815b883e444a7dd1720a6a
Paulongf <paulongf@hotmail.com>
2025-05-20 15:20:34 +0100

    Implementing LogoutUser

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -27,6 +27,16 @@ export const loginUser = createAsyncThunk('/auth/login',
   }
 );
 
+export const logoutUser = createAsyncThunk('/auth/logout',
+  async() => {
+      const response = await axios.post('http://localhost:5000/api/auth/logout', {},
+      {
+          withCredentials: true
+      })
+      return response.data;
+  }
+);
+
 export const checkAuth = createAsyncThunk(
   "/auth/checkauth",
 
@@ -92,6 +102,11 @@ const authSlice = createSlice({
           state.isAuthenticated = action.payload.success;
         })
         .addCase(checkAuth.rejected, (state, action) => {
+          state.isLoading = false;
+          state.user = null;
+          state.isAuthenticated = false;
+        })
+        .addCase(logoutUser.fulfilled, (state, action) => {
           state.isLoading = false;
           state.user = null;
           state.isAuthenticated = false;
